6bff2b25026c226ede1e8d526aab4bc600ace724
Kathryn Killebrew <kkillebrew@azavea.com>
2016-03-16 14:22:54 -0400

    0.2.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ PostgreSQL json field support for Django.
 
 setup(
     name="djsonb",
-    version="0.2.1",
+    version="0.2.2",
     url="https://github.com/azavea/djsonb",
     license="BSD",
     platforms=["OS Independent"],
